27908e2dea1f951785dc1b0cd2c7dabaff734079
constmoon <jiiw.kang@gmail.com>
2018-06-08 14:50:20 +0900

    mainpage WIP

---
+++ front/script_mainpage.js
@@ -0,0 +1,6 @@
+$('.mail-client__email-item').click( function() {
+   $(this).removeClass('.mail-client__email-item').addClass('mail-client__email-item selected');
+    var text = 
+    $('h4.mail-client__email-detail__title').text()
+//    $('p.mail-client__email-item__preview').text('바뀌나요');
+})
